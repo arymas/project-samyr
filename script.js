2a50ba98620ee22d3d34a7d9fb3664c4771112eb
arymas <129399680+arymas@users.noreply.github.com>
2023-05-22 12:06:04 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,119 @@
+import Sound from "./sounds.js";
+import Timer from "./elements.js ";
+
+import {
+  buttonPlay,
+  buttonPause,
+  buttonSet,
+  buttonSoundOff,
+  buttonSoundOn,
+  minutesDisplay,
+  secondsDisplay,
+  elements,
+} from "./sounds.js";
+
+const sounds = Sound();
+
+const buttonPlay = document.querySelector(".play");
+const buttonPause = document.querySelector(".pause");
+const buttonStop = document.querySelector(".stop");
+const buttonSet = document.querySelector(".set");
+const buttonSoundOn = document.querySelector(".sound-on");
+const buttonSoundOff = document.querySelector(".sound-off");
+const minutesDisplay = document.querySelector(".minutes");
+const secondsDisplay = document.querySelector(".seconds");
+let minutes = Number(minutesDisplay.textContent);
+let timerTimeOut;
+
+function resetControls() {
+  buttonPlay.classList.remove("hide");
+  buttonPause.classList.add("hide");
+  buttonSet.classList.remove("hide");
+  buttonStop.classList.add("hide");
+}
+
+function updateTimerDisplay(minutes, seconds) {
+  minutesDisplay.textContent = String(minutes).padStart(2, "0");
+  secondsDisplay.textContent = String(seconds).padStart(2, "0");
+}
+
+function resetTimer() {
+  updateTimerDisplay(minutes, 0);
+  clearTimeout(timerTimeOut);
+}
+
+function countdown() {
+  timerTimeOut = setTimeout(function () {
+    let seconds = Number(secondsDisplay.textContent);
+    let minutes = Number(minutesDisplay.textContent);
+
+    updateTimerDisplay(minutes, 0);
+
+    if (IsFinished) {
+      resetControls();
+      updateDisplay();
+      Sounds().timeEnd;
+      return;
+    }
+
+    if (seconds <= 0) {
+      seconds = 3;
+      --minutes;
+    }
+
+    updateTimerDisplay(minutes, String(seconds - 1));
+
+    countdown();
+  }, 1000);
+}
+
+const controls = Controls({});
+
+const timer = Timer({
+  minutesDisplay,
+  secondsDisplay,
+  resetControls: controls.reset,
+});
+
+const sound = Sound();
+
+buttonPlay.addEventListener("click", function () {
+  controls.play();
+  timer.countdown();
+  sound.pressButton();
+});
+
+buttonPause.addEventListener("click", function () {
+  controls.play();
+  timer.countdown();
+  sound.pressButton();
+});
+
+buttonStop.addEventListener("click", function () {
+  resetControls();
+  resetTimer();
+  sound.pressButton();
+});
+
+buttonSoundOff.addEventListener("click", function () {
+  buttonSoundOn.classList.remove("hide");
+  buttonSoundOff.classList.add("hide");
+  sound.bgAudio.pause();
+});
+
+buttonSoundOn.addEventListener("click", function () {
+  buttonSoundOn.classList.add("hide");
+  buttonSoundOff.classList.remove("hide");
+  sound.bgAudio.play();
+});
+
+buttonSet.addEventListener("click", function () {
+  let newMinutes = prompt("Quantos minutos?");
+  if (!newMinutes) {
+    resetTimer();
+    return;
+  }
+
+  minutes = newMinutes;
+  updateTimerDisplay(minutes, 0);
+});
